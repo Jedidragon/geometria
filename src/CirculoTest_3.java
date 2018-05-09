2d5f52c7b9834040a6d333037c92b5f410bb1d8b
Jose Manuel <josemanuel.pla@gmail.com>
2018-05-09 14:15:42 +0200

    Version de prueba

---
+++ src/CirculoTest_3.java
@@ -0,0 +1,83 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import org.junit.After;
+
+import org.junit.Before;
+
+
+
+public class CirculoTest_3  {
+
+	
+
+	private Circulo_JMPR Circulo;
+
+	private double res;
+
+	
+
+	
+
+	
+
+	@Before
+
+	public void inicio() {
+
+	Circulo=new Circulo_JMPR(-3.0,"circulo");
+
+	}
+
+	
+
+	@After
+
+	public void fin () {
+
+		Circulo=null;
+
+	}
+
+	
+
+	@Test
+
+	public void testCirculo_JMPR() {
+
+		assertNotNull(Circulo);
+
+	}
+
+	
+
+	@Test
+
+	public void testArea() {
+
+		res=Circulo.area();
+
+		System.out.print(res);
+
+		assertEquals(28.2744,res,0);
+
+	}
+
+
+
+	@Test
+
+	public void testPerimetro() {
+
+		res=Circulo.perimetro();
+
+		System.out.print(res);
+
+		assertEquals(-18.8496,res,0);
+
+		}
+
+
+
+}
